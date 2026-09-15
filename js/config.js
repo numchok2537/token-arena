abbfca74c8338d1a9035375c2d7084d3aad31119
numchok2537 <numchok2537@users.noreply.github.com>
2026-09-15 01:17:06 +0000

    Connect to deployed Apps Script endpoint

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // วางลิงก์ Web App ของ Google Apps Script ที่นี่ หลัง deploy แล้ว (ดูวิธีทำใน apps-script/README.md)
 const CONFIG = {
-  API_URL: "",
+  API_URL: "https://script.google.com/macros/s/AKfycbwiIOyOKiFW1xxYPSQuOW-9qnV0SG2FdYwNBYVJ4BYVkhxuFu-unanzNg4Vvl68SX3t/exec",
   POLL_MS: 2000,
 };
